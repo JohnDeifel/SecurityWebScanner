7bbf51fd958139a89ed00481eae0e0a73fb12411
kmonahan2 <123402763+kmonahan2@users.noreply.github.com>
2023-09-12 19:54:34 -0500

    Update script.js

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,3 +1,8 @@
+// import DOM?
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
 // Access the HTML content of the current web page
 const htmlContent = document.documentElement.outerHTML;
 const parser = new DOMParser();
