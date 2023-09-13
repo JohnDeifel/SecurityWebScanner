5b433e25d199fd1f3cf38dcda418a207f5969543
Na'im Muhammad <naimqm@gmail.com>
2023-09-12 19:51:21 -0500

    Fix basic syntax issues with script.js file

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -2,17 +2,19 @@
 const htmlContent = document.documentElement.outerHTML;
 const parser = new DOMParser();
 const doc = parser.parseFromString(htmlContent, "text/html");
-const rootElement = doc.document;
+const rootElement = doc.documentElement; // Changed from doc.document to doc.documentElement
 const linkEls = rootElement.querySelectorAll('a');
 const linkArray = Array.from(linkEls);
 
-for(let i = 0; i < linkArray.length; i++){
-    if (toLowerCase(linkArray[i].textContent) === 'google.com'){
+const nonGoogleLinks = []; // Create an array to store non-Google links
+
+for (let i = 0; i < linkArray.length; i += 1) {
+    if (linkArray[i].textContent.toLowerCase() === 'google.com') {
         console.log('Welcome to Google!');
-    }
-    else{
+    } else {
         console.log('This is not Google');
-        // add link to list
+        nonGoogleLinks.push(linkArray[i].textContent); // Add link to the list
     }
 }
 
+console.log('Non-Google Links:', nonGoogleLinks); // Print the list of non-Google links
