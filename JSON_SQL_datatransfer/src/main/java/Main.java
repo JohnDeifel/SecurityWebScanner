3d29a585829a485f391c5c361fa8f4490bef548d
Verica Karanakova <77712531+verica-kar@users.noreply.github.com>
2023-10-24 20:15:38 -0500

    thingzz

---
+++ JSON_SQL_datatransfer/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+
+import com.databasetest.DBC;
+
+public class Main {
+    public static void main(String[] args){
+        String jsonFilePath = "JSON_SQL_datatransfer/jsonfiles/userdata.json";
+
+        DBC connection = new DBC();
+        connection.dbc(jsonFilePath);
+    }
+}
