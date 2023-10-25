503d580353e91ff932e7ea8022693606dde8d44a
lordtoasty1114 <kaustgen@hawk.iit.edu>
2023-10-25 15:41:06 -0500

    Added comments to DatabaseClass

--- JSON_SQL_datatransfer/src/main/java/com/databasetest/DatabaseClass.java
+++ JSON_SQL_datatransfer/src/main/java/com/databasetest/DatabaseClass.java
@@ -1,6 +1,6 @@
 /*
 Author : Allysa Cao
-Contributor: Verica Karanakova
+Contributor: Verica Karanakova, Kaleb Austgen
 Date Created: 10-16-2023
 DatabaseClass: connection to the database with JsonArray
  */
@@ -17,25 +17,34 @@ import com.google.gson.JsonArray;
 /**
  *
  * @author dieucao3011
+ * Edited 25th October
+ * Takes three strings needed to connect to the database, the URL, username and password
+ * Prepares the SQL statement to add values into the database by feeding it a SQL command to add a row
  */
 public class DatabaseClass {
     private String url;
     private String username;
     private String password;
     
+    //Constructor
     public DatabaseClass(String url, String username, String password) {
         this.url = url;
         this.username = username;
         this.password = password;
     }
+
+    //Method that takes a jsonArray and array of strings
     public void insertDataFromJson(JsonArray jsonArray, String[] dataArray) {
     try {
+        //Creates the connection using the given url, username and password
         Connection connection = DriverManager.getConnection(url, username, password);
         System.out.println("Connected to the database");
 
+        //Prepares the SQL statement to send to the server
         String sql = "INSERT INTO UserData (WebsiteName, IPAddress, Domain, TimeAccessed, LocationAccessed, ReasonForBlock) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement statement = connection.prepareStatement(sql);
 
+        //Formats the data
         for (String data : dataArray) {
             var dataParts = data.split(",");
             for (int i = 1; i <= 6; i++) {
