207194c63ad63eea836f9210065f4ea3ad968fa1
Na'im Muhammad <naimqm@gmail.com>
2023-10-31 19:03:49 -0500

    Restore script.js

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,140 +1,110 @@
 {
-var domain;
-var timeAccessed;
-var location;
-var pageTitle;
-var rating = 5; // out of 5 stars
-
-function fetchData() {
-  //domain = pass
+  var domain;
+  var timeAccessed;
+  var location;
+  var pageTitle;
+  var rating = 5; // out of 5 stars
   
-  let event = new Date();
-  timeAccessed = event.toString();
-
-  //location = pass
-
-  pageTitle = document.title;
-  pageURL = window.location.href;
-};
-
-// https://gist.github.com/amundo/3951b04c1e0725445774 (OLD, WE'RE NOT USING THIS ANYMORE)
-function saveJSON(data){
-  return JSON.stringify(data, null, 2);
-};
-
-// send to sql server with fetch api
-// add link to server, not the database
-const url2 = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:ipro497.database.windows.net,1433;Database=iprowebscanner;Uid=test;Pwd={Shambhawi@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'; 
-
-fetch(url2, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: blob,
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+  function fetchData() {
+    //domain = pass
+    
+    let event = new Date();
+    timeAccessed = event.toString();
+  
+    //location = pass
+  
+    pageTitle = document.title;
+    pageURL = window.location.href;
+  
+    //rating = pass
+  };
+  
+  // https://gist.github.com/amundo/3951b04c1e0725445774
+  function saveJSON(data){
+    return JSON.stringify(data, null, 2);
+    // BELOW IS COMMENTED OUT BECAUSE ALL WE NEED IS AN OBJECT (FOR NOW)
+  
+    /*var blob = new Blob([stringified], {type: "application/json"});
+    var url = URL.createObjectURL(blob);
+    
+    
+    var a = document.createElement('a');
+    a.download = saveAs + '.json';
+    a.href = url;
+    a.id = saveAs;
+    document.body.appendChild(a);
+    a.click();
+    document.querySelector('#' + a.id).remove();*/
+  };
+  
+  // Return the html of the page
+  function getHTML() {
+    pageHTML = document.documentElement.outerHTML;
+    return pageHTML;
+  };
+  
+  // Get all links on the page
+  function getLinks() {
+    var links = document.getElementsByTagName('a');
+    var linksArray = [];
+    for (var i = 0; i < links.length; i++) {
+      linksArray.push(links[i].href);
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Response from server:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-};
-
-// Return the html of the page
-function getHTML() {
-  pageHTML = document.documentElement.outerHTML;
-  return pageHTML;
-};
-
-// Get all links on the page
-function getLinks() {
-  var links = document.getElementsByTagName('a');
-  var linksArray = [];
-  for (var i = 0; i < links.length; i++) {
-    linksArray.push(links[i].href);
-  }
-  
-  return linksArray;
-};
-
-// Return true if URL is https otherwise false
-function isHttps() {
-  if(window.location.protocol === 'https:'){
-    // return true;
-  } else {
-    rating -= 1;
-    // return false;
-  };
-};
-
-// Return true if URL is shortened, false if not shortened
-function isShortened() {
-  pageURL = window.location.href;
-  if ((pageURL.includes('bit.ly')) || (pageURL.includes('tinyurl'))){
-    rating -= 1;
-    // return true;
-  } /* else {
-    return false;
-  }*/;
-};
-
-// Return true if URL includes @ symbol (common phishing tactic)
-function hasAt() {
-  pageURL = window.location.href;
-  if (pageURL.includes('@')){
-    rating -= 1;
-    // return true;
-  } /* else {
-    return false;
-  }*/;
-};
-
-// TODO: Fetch the user's IP address
-
-// TODO: Fetch the user's location
-
-// Refresh the data when a new link is accessed
-window.onload = function() {
-  window.onload = null;
-  fetchData();
-  isHttps();
-  isShortened();
-  hasAt();
-  if (rating < 0){
-    rating = 0;
-  };
-  if (rating <= 3){
-    window.alert("Page is insecure. Star Rating: " + rating);
-  }
-  else{
-    window.alert("Page is secure. Star Rating: " + rating);
-  }; // in the future we have to make sure nothing happens if the page is secure
-  const dataArray = {
-    eventTime: timeAccessed,
-    domainTitle: pageTitle,
-    domainURL: pageURL,
-    domainSecure: isSecure(),
-    domainLinks: getLinks(),
+    
+    return linksArray;
   };
   
-  console.log(saveJSON(dataArray));
-  // add condition to only saveJSON is rating is below acceptable
-  // saveJSON(dataArray, 'log')
-};
-
-
-
-}
-
-
-/*
-Apparently we have to ensure that CORS (Cross-Origin Resource Sharing) settings on our Azure server allows requests from the origin of our JavaScript application, that is something I will work on shortly.
-*/
-
+  // Return true if URL is https otherwise false
+  function isSecure() {
+    if(window.location.protocol === 'https:'){
+      return true;
+    } else {
+      rating = rating - 2.5;
+      return false;
+    };
+  };
+  
+  // Return true if URL is  shorted, false if not shortened
+  function isShortened() {
+    pageURL = window.location.href;
+    if ((pageURL.includes('bit.ly')) || (pageURL.includes('tinyurl'))){
+      rating -= 2.5;
+      return true;
+    } else {
+      return false;
+    };
+  };
+  // TODO: Fetch the user's IP address
+  
+  // TODO: Fetch the user's location
+  
+  // Refresh the data when a new link is accessed
+  window.onload = function() {
+    window.onload = null;
+    fetchData();
+    if (rating < 0){
+      rating = 0;
+    }
+    if (!(isSecure()) || (isShortened())){
+      window.alert("Page is insecure.");
+    }
+    else{
+      window.alert("Page is secure.");
+    }
+    const dataArray = {
+      eventTime: timeAccessed,
+      domainTitle: pageTitle,
+      domainURL: pageURL,
+      domainSecure: isSecure(),
+      domainLinks: getLinks(),
+    }
+    
+    console.log(saveJSON(dataArray))
+    // add condition to only saveJSON is rating is below acceptable
+    // saveJSON(dataArray, 'log')
+  };
+  
+  
+  
+  }
+  
