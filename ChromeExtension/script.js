6d89ea68e0b7b19cd5076001591802a05320f306
lcostawang <112441843+lcostawang@users.noreply.github.com>
2023-11-21 21:17:37 -0600

    Update script.js
    Look, I'd add a real commit name here but I'm tired. Sorry.

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,5 +1,5 @@
 {
-  // Data variables (mostly for Backend)
+  // Data variables (mostly for Backend)shortStringat
   var timeAccessed;
   var pageTitle;
   var pageURL;
@@ -8,10 +8,14 @@
   var userCountry;
 
   // Security variables (for report)
-  var https = "";
-  var shortened = "";
-  var at = "";
-  var extension = "";
+  var httpsString = "";
+  var shortString = "";
+  var atString = "";
+  var extensionString = "";
+  var httpsUnsafe = false;
+  var shortUnsafe = false;
+  var atUnsafe = false;
+  var extensionUnsafe = false;
 
   var rating = 5; // out of 5 stars
   
@@ -21,43 +25,10 @@
     pageTitle = document.title;
     pageURL = window.location.href;
   };
-
-  // Helper function for fetchIPData()
-  // Author: Na'im
-  /* function json(url) {
-    return fetch(url).then(res => res.json());
-  }; */
-
-  // Co-authors: Na'im, Lucas
-  /* function fetchIPData() {
-    let apiKey = 'ccaa3a53c5c11195be3f0f03e7ab1d13180c05bc1d50086ee8fd50b8';
-    json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-    userIP = data.ip; // CURRENTLY NOT WORKING
-    userLocation = data.city; // CURRENTLY NOT WORKING
-    userCountry = data.country_code; // CURRENTLY NOT WORKING
-    });
-  }; */
   
   function makeJSON(data) {
     return JSON.stringify(data, null, 2);
   };
-  
-  // Return the html of the page (unused)
-  /* function getHTML() {
-    pageHTML = document.documentElement.outerHTML;
-    return pageHTML;
-  }; */
-  
-  // Get all links on the page (unused)
-  /* function getLinks() {
-    var links = document.getElementsByTagName('a');
-    var linksArray = [];
-    for (var i = 0; i < links.length; i++) {
-      linksArray.push(links[i].href);
-    }
-    return linksArray;
-  }; */
-  
 
   // SECURITY CHECKS
 
@@ -66,7 +37,8 @@
   function isNotHttps() {
     if(window.location.protocol !== 'https:'){
       rating -= 1;
-      https = "- This URL does not follow HTTPS protocol. HTTPS guarantees a secure connection.\n";
+      httpsUnsafe = true;
+      httpsString = "- This URL does not follow HTTPS protocol. HTTPS guarantees a secure connection.\n";
     }
   };
   
@@ -76,7 +48,8 @@
     pageURL = window.location.href;
     if ((pageURL.includes('bit.ly')) || (pageURL.includes('tinyurl'))){
       rating -= 1;
-      shortened = "- This URL was treated by a link shortener, possibly to hide the true URL.\n";
+      shortUnsafe = true;
+      shortString = "- This URL was treated by a link shortener, possibly to hide the true URL.\n";
     }
   };
 
@@ -86,7 +59,8 @@
     pageURL = window.location.href;
     if (pageURL.includes('@')){
       rating -= 1;
-      at = "- This URL contains an @ symbol. It could be trying to redirect you somewhere else.\n";
+      atUnsafe = true;
+      atString = "- This URL contains an @ symbol. It could be trying to redirect you somewhere else.\n";
     }
   };
 
@@ -97,7 +71,8 @@
     // based off an article on the most unsafe domain extensions (article link?)
     if ((pageURL.includes('.cf')) || (pageURL.includes('.work'))|| (pageURL.includes('.ml')) || (pageURL.includes('.ga'))|| (pageURL.includes('.gq')) || (pageURL.includes('.fit')) || (pageURL.includes('.tk'))){
       rating -= 1;
-      extension = "- Unsafe URL extension: this page is being hosted in a location associated with unsafe websites.\n";
+      extensionUnsafe = true;
+      extensionString = "- Unsafe URL extension: this page is being hosted in a location associated with unsafe websites.\n";
     }
   }
   
@@ -105,8 +80,6 @@
   window.onload = function() {
     safe = true;
     window.onload = null;
-    fetchData();
-    // fetchIPData();
     isNotHttps();
     isShortened();
     hasAt();
@@ -115,26 +88,27 @@
       rating = 0;
     }
     if (rating <= 4){
+      // Get the data for backend
+      fetchData();
+      const dataArray = {
+        eventTime: timeAccessed,
+        domainTitle: pageTitle,
+        domainURL: pageURL,
+        domainRating: rating,
+        reasonNoHttps: httpsUnsafe,
+        reasonShortened: shortUnsafe,
+        reasonAtSymbol: atUnsafe,
+        reasonBadExtension: extensionUnsafe
+      };
+      console.log(makeJSON(dataArray));
       // Show user the rating, security report, and prompt them to go back
-      if(window.confirm("This page could be unsafe; its HawkPhish Security Rating is " + rating + " stars.\n\nThis page's vulnerabilities are: (SCROLL DOWN IF NEEDED)\n" + https + shortened + at + extension + "\nWe recommend you press Cancel to return to the previous page now. If you wish to proceed at your own risk, press OK.") == false){
+      if(window.confirm("This page could be unsafe; its HawkPhish Security Rating is " + rating + " stars.\n\nThis page's vulnerabilities are: (SCROLL DOWN IF NEEDED)\n" + httpsString + shortString + atString + extensionString + "\nWe recommend you press Cancel to return to the previous page now. If you wish to proceed at your own risk, press OK.") == false){
         history.back();
       }
     }
-    else {
+    /* else {
       window.alert("This page is secure; its HawkPhish Security Rating is " + rating + " stars.");
-    }
-    // WE HAVE TO TRANSFER THE BELOW CODE INSIDE THE RATING IF STATEMENT, OTHERWISE IT WON'T RUN BEFORE THE ALERT
-    const dataArray = {
-      eventTime: timeAccessed,
-      domainTitle: pageTitle,
-      domainURL: pageURL,
-      // IPAddress: userIP, // CURRENTLY NOT WORKING
-      // location: userLocation, // CURRENTLY NOT WORKING
-      // country: userCountry, // CURRENTLY NOT WORKING
-      domainRating: rating
-    };
-    
-    // console.log(makeJSON(dataArray)); // (for testing)
+    } */
   };
   
 }
